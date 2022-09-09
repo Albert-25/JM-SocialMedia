e90e94cd4c3539eabd0a0b65b3873936729be325
Albert-25 <ajst_95@outlook.com>
2022-09-09 16:48:25 -0500

    post model created

---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./src/routes/posts.js"
+
+const app = express();
+
+app.use("/posts", postRoutes)
+
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://AlbertJM:python@cluster0.s8uxibm.mongodb.net/?retryWrites=true&w=majority";
+const PORT = 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => { console.log(`server running on port: ${PORT}`) }))
+    .catch((error) => console.log(`This is the error why your app is not working: ${error.message}`));
+
+
