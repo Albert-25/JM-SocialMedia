7e404667fafa46043f76e41a4f587d68fee98e55
Albert-25 <ajst_95@outlook.com>
2022-09-14 11:52:51 -0500

    limit to upload file added

--- api/index.js
+++ api/index.js
@@ -7,6 +7,8 @@ import postRoutes from "./src/routes/posts.js"
 const app = express();
 
 app.use(cors());
+app.use(express.json({ limit: '50mb', extended: true }))
+app.use(express.urlencoded({limit:'50mb', extended: true }))
 app.use("/posts", postRoutes)
 
 
